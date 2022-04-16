2a2b7893716f40e1c175c7cba38c1b703dd023af
JimmyGee123 <jseignior@live.com.au>
2022-04-16 23:32:32 +1000

    js

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ const checkOwner = async (account) => {
       editions = [...editions, ...data.editions]
       nextPage = data.next_page
     }
-    if(!isowner){
+    if(!iOwner){
       updateStatusText(isOwner, false)
     }
   }
