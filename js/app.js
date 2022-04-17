56f532c372fb005eea2835a2b79f73779882e449
JimmyGee123 <jseignior@live.com.au>
2022-04-17 21:48:11 +1000

    js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const checkOwner = async (account) => {
     let isOwner = false;
     let page = 1
     let data = []
-    let editions =[]
+    editions =[]
     data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
     
     isOwner = !isOwner ? data.isOwner : isOwner;
