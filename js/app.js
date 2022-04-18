0b22b438e11191d9309b324ca425b4dc83a6580d
JimmyGee123 <jseignior@live.com.au>
2022-04-18 21:41:11 +1000

    js

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ const checkOwner = async (account) => {
       isOwner = !isOwner ? data.isOwner : isOwner;
       updateStatusText(isOwner, true)
      
-      if(data.editions.length != 0){
+      if(data.editions.length < 1){
         //console.log(data.editions)
         editions = [...editions, ...data.editions]
         console.log('here')
